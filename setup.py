b7393e2eaed2c11b31a44a89d851dd087cfe2557
Sourcery AI <>
2022-08-12 13:27:39 +0000

    'Refactored by Sourcery'

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ class _TqdmUpTo(tqdm):  # type: ignore
 
 # extract dic
 filename = "dic.tar.gz"
-print('Downloading: "{}"'.format(_DICT_URL))
+print(f'Downloading: "{_DICT_URL}"')
 with _TqdmUpTo(
     unit="B",
     unit_scale=True,
@@ -189,7 +189,7 @@ with _TqdmUpTo(
 ) as t:  # all optional kwargs
     urlretrieve(_DICT_URL, filename, reporthook=t.update_to)
     t.total = t.n
-print("Extracting tar file {}".format(filename))
+print(f"Extracting tar file {filename}")
 with tarfile.open(filename, mode="r|gz") as f:
     f.extractall(path="./")
 os.remove(filename)
@@ -235,7 +235,7 @@ for s in [
 ext_modules = [
     Extension(
         name="pyopenjtalk.openjtalk",
-        sources=[join("pyopenjtalk", "openjtalk" + ext)] + all_src,
+        sources=[join("pyopenjtalk", f"openjtalk{ext}")] + all_src,
         include_dirs=[np.get_include()] + include_dirs,
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
@@ -253,13 +253,14 @@ ext_modules = [
     )
 ]
 
+
 # Extension for HTSEngine backend
 htsengine_src_top = join("lib", "hts_engine_API", "src")
 all_htsengine_src = glob(join(htsengine_src_top, "lib", "*.c"))
 ext_modules += [
     Extension(
         name="pyopenjtalk.htsengine",
-        sources=[join("pyopenjtalk", "htsengine" + ext)] + all_htsengine_src,
+        sources=[join("pyopenjtalk", f"htsengine{ext}")] + all_htsengine_src,
         include_dirs=[np.get_include(), join(htsengine_src_top, "include")],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
@@ -273,6 +274,7 @@ ext_modules += [
     )
 ]
 
+
 # Adapted from https://github.com/pytorch/pytorch
 cwd = os.path.dirname(os.path.abspath(__file__))
 if os.getenv("PYOPENJTALK_BUILD_VERSION"):
@@ -284,7 +286,7 @@ else:
             .decode("ascii")
             .strip()
         )
-        version += "+" + sha[:7]
+        version += f"+{sha[:7]}"
     except subprocess.CalledProcessError:
         pass
     except IOError:  # FileNotFoundError for python 3
@@ -299,10 +301,10 @@ class build_py(setuptools.command.build_py.build_py):
     @staticmethod
     def create_version_file():
         global version, cwd
-        print("-- Building version " + version)
+        print(f"-- Building version {version}")
         version_path = os.path.join(cwd, "pyopenjtalk", "version.py")
         with open(version_path, "w") as f:
-            f.write("__version__ = '{}'\n".format(version))
+            f.write(f"__version__ = '{version}'\n")
 
 
 class develop(setuptools.command.develop.develop):
@@ -334,7 +336,7 @@ setup(
     cmdclass=cmdclass,
     install_requires=[
         "numpy >= 1.20.0",
-        "cython >= " + min_cython_ver,
+        f"cython >= {min_cython_ver}",
         "six",
         "tqdm",
     ],
